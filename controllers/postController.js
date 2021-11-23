9d86cc169b676ee4801288f66bc64608a4ca5273
“Alex <alexandriadelaney923@gmail.com>
2021-11-23 11:33:50 -0800

    added post, put and delete routes for comments

--- controllers/postController.js
+++ controllers/postController.js
@@ -19,10 +19,6 @@ router.get("/", (req, res) => {
   });
 });
 
-// router.get('/:postId', (req,res) => {
-//     Post.find({_id: req.params.postId}).then((post) => {res.json({status:200, post:post})})
-// }
-
 //UPDATE A POST
 router.put("/:postId", (req, res) => {
   Post.findOneAndUpdate({ _id: req.params.postId }, req.body, { new: true }).then(
@@ -54,11 +50,34 @@ router.post("/:postId", (req, res) => {
     // .catch((error) => console.log(error));
 });
 
-
-//GET COMMENTS/DISPLAY COMMENTS
-
 //UPDATE A COMMENT
+router.put("/:postId/:commentId", (req, res) => {
+  Post.updateOne(
+    {
+      _id: req.params.postId,
+      "comments._id": req.params.commentId,
+    },
+    {
+      $set: { "comments.$": req.body },
+    }
+  )
+    .then((post) => res.status(201).json({ status: 201, post: post }))
+    .catch((error) => console.log(error));
+});
+
 
 //DELETE A COMMENT
+router.delete("/:postId/:commentId", (req, res) => {
+  Post.updateOne(
+    {
+      _id: req.params.postId,
+    },
+    {
+      $pull: { comments: { _id: req.params.commentId } },
+    }
+  )
+    .then((post) => res.status(205).json({ status: 201, post: post }))
+    .catch((error) => console.log(error));
+});
 
 module.exports = router;
